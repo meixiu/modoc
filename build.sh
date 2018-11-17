79d3fa1939704ee2181c9bc5de73f2d4e6d4a4a0
hsyu <hsyu@yoozoo.com>
2018-11-17 22:47:16 +0800

    no message

--- build.sh
+++ build.sh
@@ -4,14 +4,16 @@ path=$(cd "$(dirname "$0")"; pwd)
 # Mac
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bin/modoc-mac -v main.go
 chmod +x bin/modoc-mac
+zip 'bin/modoc-mac.zip' 'bin/modoc-mac' 'bin/README.pdf'
 
 # linux
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/modoc-linux -v main.go
-chmod +x bin/modoc-linux
+#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/modoc-linux -v main.go
+#chmod +x bin/modoc-linux
 
 # Win
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/modoc-win.exe -v main.go
 chmod +x bin/modoc-win.exe
+zip 'bin/modoc-win.zip' 'bin/modoc-win.exe' 'bin/README.pdf'
 
 
 
